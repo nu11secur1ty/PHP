fe3f83e61e9267bc31c4179ba8e56d7accebbe0e
nu11secur1ty <venvaropt@gmail.com>
2020-05-03 15:24:00 +0300

    Create sender.php

---
+++ send_mail/sender.php
@@ -0,0 +1,14 @@
+<?php
+   //sending to someone
+   $to_email = "to_someone_@gmail.com";
+   $subject = "Simple Email Test via PHP";
+   $body = "Hi,nn This is test email send by PHP Script";
+   // from someone
+   $headers = "From: cyberone@example.com";
+ 
+   if ( mail($to_email, $subject, $body, $headers)) {
+      echo("Email successfully sent to $to_email...\n");
+   } else {
+      echo("Email sending failed...\n");
+   }
+?>
